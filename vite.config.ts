27f6ee2b852c680d12f1cb6dbf97ccc907f69be8
Luiz <seu-email@example.com>
2024-07-17 17:31:11 -0300

    update vite2

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: '/subpath/', // ajuste o caminho conforme necessário
+  base: '/', // ajuste o caminho conforme necessário
 })
