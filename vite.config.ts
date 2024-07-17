c03c93c7fff41e234a02d1029f5f210e46a3b695
Luiz <seu-email@example.com>
2024-07-17 17:21:42 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+  base: '/subpath/', // ajuste o caminho conforme necessário
+})
