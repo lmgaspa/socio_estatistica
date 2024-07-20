f6dd89704b654d4121b9a1b612e6cafdb20a9312
Luiz <seu-email@example.com>
2024-07-20 16:12:40 -0300

    final

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,19 @@
 <template>
   <footer class="p-4 bg-color1 text-white text-center">
     <h1 class="text-3xl font-bold mb-4">Entre em contato conosco</h1>
-    <div class="flex items-center justify-center space-x-2">
+    <div class="flex items-center justify-center space-x-2 hover:text-colorText_h1 cursor-pointer">
       <font-awesome-icon :icon="['fas', 'phone']" class="w-6 h-6" />
       <p>(73) 3212-1229</p>
     </div>
-    <div class="flex items-center justify-center space-x-2 mt-2">
+    <a href="mailto:sociop@gmail.com" class="flex items-center justify-center space-x-2 mt-2 hover:text-colorText_h1 cursor-pointer">
       <font-awesome-icon :icon="['fas', 'envelope']" class="w-6 h-6" />
       <p>sociop@gmail.com</p>
-    </div>
-    <div class="flex items-center justify-center space-x-2 mt-2">
+    </a>
+    <a href="https://wa.me/73988698660" target="_blank" class="flex items-center justify-center space-x-2 mt-2 hover:text-colorText_h1 cursor-pointer">
       <font-awesome-icon :icon="['fab', 'whatsapp']" class="w-6 h-6" />
       <p>(73) 98869-8660</p>
-    </div>
-    <p class="mt-4">Endereço: Rua Francisco Ribeiro Jr., 198, Sala 606 - Centro, Itabuna - BA.</p>
+    </a>
+    <p class="mt-4 hover:text-colorText_h1 cursor-pointer">Endereço: Rua Francisco Ribeiro Jr., 198, Sala 606 - Centro, Itabuna - BA.</p>
   </footer>
 </template>
 
